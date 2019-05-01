bebb2a420c1c8eac875721112d28119cd1353c1f
Jo <josephine.yt.chong@gmail.com>
2019-05-01 15:35:15 +1000

    Added Enum
    Added enum to listing model (purchased, pending, delivered)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+  enum status: [:purchased, :pending, :delivered]
   has_one :transaction, dependent: :destroy
   has_many :products, through: :listings_product, dependent: :destroy
   has_one :delivery, dependent: :destroy

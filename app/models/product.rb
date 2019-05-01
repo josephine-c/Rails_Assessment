02f481a2abe76ca54a53fffbedbbe09fb53c5af3
Jo <josephine.yt.chong@gmail.com>
2019-05-01 15:11:52 +1000

    Active record relationships in model
    Active record relationships in model for pictures, in users, products and shops

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :shop
   has_many listings, through: :listings_product, dependent: :destroy
+  has_one_attached :picture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_one :contact, as: :contactable, dependent: :destroy
   has_many :listings, dependent: :destroy
   has_many :delveries, dependent: :destroy
+  has_one_attached :picture
 end

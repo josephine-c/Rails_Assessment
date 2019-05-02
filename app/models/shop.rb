d1c82f698a71bf87b1c33adfef52f68e0481c5f6
Jo <josephine.yt.chong@gmail.com>
2019-05-02 11:11:35 +1000

    Picture to Image in models
    use :image, as :imageable instead of :picture

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,5 +2,5 @@ class Shop < ApplicationRecord
     has_one :contact, as: :contactable, dependent: :destroy
     has_many :listings, dependent: :destroy
     has_many :products, dependent: :destroy
-    has_many_attached :pictures
+    has_many_attached :images, as: :imageable
 end

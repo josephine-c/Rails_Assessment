3e0204c4ade4b79dedc6edd5c5335152a173c83b
Bryce Harlum <bryce.harlum@gmail.com>
2019-05-02 15:17:36 +1000

    Updated routes for user
    allows for sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_up: "join"}
   resources :listings, :shops
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#index", as: "root"

25bc2c1e6f6c35753da29ddde0c9c7d7cc6ea2b4
Jo <josephine.yt.chong@gmail.com>
2019-05-03 17:03:32 +1000

    Add size enum
    Add size enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :shop
   has_many :listings, through: :listings_product, dependent: :destroy
   has_many_attached :pictures
+  enum size: [:xsmall, :small, :medium, :large, :xlarge, :onesize]
 end

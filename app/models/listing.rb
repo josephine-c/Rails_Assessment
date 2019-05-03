58d5ea6cb9f9679431a52adb4e914d45be02d72c
Jo <josephine.yt.chong@gmail.com>
2019-05-03 10:02:59 +1000

    Add more enums
    Add more enums :pending, :paid, :accepted, :purchased, :delivered

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :shop
-  enum status: [:purchased, :pending, :delivered]
+  enum status: [:pending, :paid, :accepted, :purchased, :delivered]
   has_one :order, dependent: :destroy
   has_many :products, through: :listings_product, dependent: :destroy
   has_one :delivery, dependent: :destroy

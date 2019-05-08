73a8072fc4bc34981dfa8494bfcba36e3c9adfe9
Jo <josephine.yt.chong@gmail.com>
2019-05-08 12:14:10 +1000

    Added the lists for the user login page
    Added the lists for the user login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,9 @@ class PagesController < ApplicationController
         if current_user != nil
             # grabbing all listings that have a user id matching current user.
             @lists = Listing.where(user_id: current_user.id)
+    
             # grab all deliveries(courier jobs) that match current user id.
-            # p @jobs = Listing.find(Deilvery.where(user_id: current_user.id).listing_id)
+            # @jobs = Listing.find(Deilvery.where(user_id: current_user.id).listing_id)
         end
     end
 end
